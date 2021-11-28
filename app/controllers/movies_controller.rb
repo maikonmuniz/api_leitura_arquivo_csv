7cd57a0091d51b6010b41e82231a59b5c15b64e6
Maikon Muniz <maikon.muni@yahoo.com.br>
2021-11-28 13:17:06 -0300

    adicionando rubocop na aplicação

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,78 +1,66 @@
+# frozen_string_literal: true
+
 class MoviesController < ApplicationController
+  def index
+    # ordenar os registros por ano
+    @movies = Movie.all.order(:year)
 
-    def index
+    # filtrar os registro por ano, genero e país
+    filter_by_year if params[:year]
+    filter_by_genre if params[:genre]
+    filter_by_country if params[:country]
 
-      #ordenar os registros por ano
-      @movies = Movie.all.order(:year)
-
-
-      #filtrar os registro por ano, genero e país
-      filter_by_year if params[:year]
-      filter_by_genre if params[:genre]
-      filter_by_country if params[:country]
-
-      render json: @movies, except: [:created_at, :updated_at]
-
-    end
-  
-  
-  #criar os registro no banco de dados com um endpoint diferente que foi pedido
-  def create
-
-
-    require "CSV"
-    require 'time'
-    require 'date'
-
-    file = 'tmp/netflix_titles.csv'
-    saida = CSV.read(file)
-
-    def date_modify(date_list)
-
-        Date.parse(date_list).to_s
-
-    end
-
-    #esse variavel é para pegar todos os registros apartir do indice 1
-    final = saida[1..]
-
-
-    # esse if verifica se há dados existentes no banco, poderia registrar Movie.exists?(id)
-    if Movie.exists?
-
-     @movies = ["O Arquivo csv já foi registrado no banco de dados"]
-
-    else
-      
-      for i in final do
-
-        @movies = Movie.create!(title: i[2], genre: i[1], year: i[7], country: i[5], published_at: date_modify(i[6]), description: i[10])
-        
-      end
-
-    end
-
-    render json: @movies
-    
+    render json: @movies, except: %i[created_at updated_at]
   end
 
-  #filtros
+  def file_csv(file)
+    require 'CSV'
+    CSV.read(file)
+  end
+
+  def modify_date(date_netflix)
+    require 'time'
+    require 'date'
+    Date.parse(date_netflix).to_s
+  end
+
+  def indice(indice_just)
+    indice_just[1..]
+  end
+
+  def validate(data)
+    if Movie.exists?
+      render_return = { status: :unprocessable_entity, message: 'Dados já forar cadastrados!' }
+    else
+      data.each do |i|
+        @movies = Movie.create!(title: i[2], genre: i[1], year: i[7], country: i[5],
+                                published_at: modify_date(i[6]), description: i[10])
+      end
+
+      render_return = { status: :created, message: 'Dados registrados!' }
+    end
+
+    render_return
+  end
+
+  def create
+    data = file_csv('tmp/netflix_titles.csv')
+    data = indice(data)
+    render_return = validate(data)
+
+    render json: render_return
+  end
+
+  # filtros
   def filter_by_year
-
     @movies = @movies.where(year: params[:year])
-
   end
 
   def filter_by_genre
-
     @movies = @movies.where(genre: params[:genre])
-
   end
 
   def filter_by_country
-
     @movies = @movies.where(country: params[:country])
-
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'movies/index'
-  get 'movies/show'
-  get 'movies/create'
+
+  post 'movies/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
